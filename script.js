e798d2fc429011e37006dce73cf320ef9cc33731
Vladimir0657305 <vv0657305@gmail.com>
2022-08-09 19:22:21 +0300

    create slider

---
+++ script.js
@@ -0,0 +1,31 @@
+let slideindex = 1;
+showslides(slideindex);
+
+function plusSlides(n) {
+    showslides(slideindex += n);
+}
+
+function currentSlide(n) {
+    showslides(slideindex = n);
+}
+
+function showslides(n) {
+    let i;
+    let slides = document.getElementsByClassName("myslides");
+    let dots = document.getElementsByClassName("dot");
+    // console.log(slideindex);
+    if (n > slides.length) {
+        slideindex = 1;
+    }
+    if (n < 1) {
+        slideindex = slides.length;
+    }
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = 'none';
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace('active', '');
+    }
+    slides[slideindex - 1].style.display = 'inline-block';
+    dots[slideindex - 1].style.display = 'inline-block';
+}
